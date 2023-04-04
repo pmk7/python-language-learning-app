3a0483c21c02df390041f0fc73584f6ddea6d18b
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-04-04 10:28:55 +0200

    initital commit

---
+++ file_conversion.py
@@ -0,0 +1,45 @@
+import json
+
+output_file = 'my_vocab.json'
+my_words = []
+
+
+def line_to_dict(split_line):
+    line_dict = {}
+    for part in split_line:
+        key, value = part.split(":")
+        line_dict[key] = value
+
+    return line_dict
+
+
+def convert():
+    f = open("my_vocab.txt", "r", encoding="utf-8")
+    content = f.read()
+    f.close()
+    splitcontent = content.splitlines()
+  
+    adjusted_content = [x.replace("\t", " : ").replace('.', '') for x in splitcontent]
+    
+    for i, word in enumerate(adjusted_content):
+        word_id = i + 1
+        eng_word, ger_word = word.split(" : ")
+    
+        word_dict = {'id': word_id, 'german': ger_word, 'english': eng_word, 'correct': 0, 'incorrect': 0}
+        my_words.append(word_dict)
+        
+        
+    
+
+    # id_words = [(i +1, word) for i, word in enumerate(adjusted_content)]  
+   
+    print(my_words[1])
+
+    # rs = json.dumps(dict(word_dict), ensure_ascii=False)
+    # print(rs)
+    
+    
+   
+    
+    
+convert()
