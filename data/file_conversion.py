8be73bd6cd1af52a14b25636e32b637ffcbffefe
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-04-24 17:32:39 +0100

    created BaseUser class and two subclasses, RegularUser and PremiumUser plus user dictionaries

--- data/file_conversion.py
+++ data/file_conversion.py
@@ -1,4 +1,10 @@
+import json
+
 my_words = []
+
+def save_vocab_to_json(vocab, filepath):
+    with open(filepath, 'w', encoding='utf-8') as file:
+        json.dump(vocab, file, ensure_ascii=False, indent=4)
 
 def convert():
     f = open("data/my_vocab.txt", "r", encoding="utf-8")
@@ -19,6 +25,8 @@ def convert():
     return my_words
     
 
-
    
-my_words = convert()    
+my_words = convert()   
+# save_vocab_to_json(my_words, "my_words.json")
+print(save_vocab_to_json(my_words, "my_words.json")) 
+
--- main.py
+++ main.py
@@ -1,59 +1,141 @@
 import os
 import random
+import json
 from sen_gen import SentenceGenerator
 from data import *
 
 api_key = os.getenv("OPENAI_API_KEY")
 selected_word_example = SentenceGenerator(api_key)
 
+data_directory = "user_data"
+if not os.path.exists(data_directory):
+    os.makedirs(data_directory)
+
+class BaseUser:
+    def __init__(self, username, filepath):
+        self.username = username
+        self.filepath = f"{filepath}/{self.username}.json"
+        
+        
+        
+        if not os.path.exists(self.filepath):
+            self.vocab = []
+            self.save_vocab()
+        else:
+            self.load_vocab()
+
+    def load_vocab(self):
+        with open(self.filepath, 'r') as file:
+            self.vocab = json.load(file)
+          
+    def save_vocab(self):
+        with open(self.filepath, 'w') as file:
+            json.dump(self.vocab, file)
+    
+    def print_my_vocab(self):
+      print(self.vocab)
+
+    def find_word(self, german):
+        print(self.vocab)
+        for word in self.vocab:
+            if word['german'] == german:
+                return word
+        return None
+
+
+    def add_word(self, german, english):
+      if self.find_word(german):
+        print('Already contained!') 
+      else:
+        word_id = len(self.vocab) + 1
+        word = {'id': word_id, 'german': german, 'english': english, 'correct': 0, 'incorrect': 0}
+        self.vocab.append(word)
+        self.save_vocab()
+
+
+    def remove_word(self, german):
+        word = self.find_word(german)
+        if word:
+            self.vocab.remove(word)
+            self.save_vocab()
+            
+            
+    def update_my_vocab(self, german, english):
+        word_id = len(self.vocab) + 1
+        word = {'id': word_id, 'german': german, 'english': english, 'correct': 0, 'incorrect': 0}
+        self.save_vocab()
+        
+            
+
+    def __str__(self):
+        return f"User: {self.username}, Vocab: {len(self.vocab)}"
+      
+      
+    
+class RegularUser(BaseUser):
+    def __init__(self, username, filepath):
+        super().__init__(username, filepath)
+        self.quiz_rounds = 1
+   
+          
+    def __str__(self):
+      return f"Regular User: {self.username}, Vocab: {len(self.vocab)}"     
+
+
+
+class PremiumUser(BaseUser):
+    def __init__(self, username, filepath):
+        super().__init__(username, filepath)
+        self.quiz_rounds = 5
+ 
+    def __str__(self):
+      return f"Premium User: {self.username}, Vocab: {len(self.vocab)}"       
+
+
+
+
 class Quiz:
-  def __init__(self, vocab):
+  def __init__(self, vocab, user):
     self.vocab = vocab
+    self.user = user
     
-    print("Welcome to the German Quiz!")
     
+    print("Welcome to the German Quiz!\n")
+
+  
   def random_word(self):
     random_num = random.randint(0, len(self.vocab) - 1)
     random_word = self.vocab[random_num]
     ger_word = random_word.get('german')
     
-    answer = input(f"What is the English translation of {ger_word}? Press 'h' for a hint:  ")
+    answer = input(f"What is the English translation of {ger_word}? Press 'h' for a hint:  \n" )
     
     if answer == 'h':
       word = SentenceGenerator(api_key)
-      print(word.generate_sentence(ger_word))
+      print(word.generate_sentence(ger_word)+ "\n")
       answer = input("Answer: ")
       
     if answer == random_word.get('english'):
       print("Correct!")
     elif answer != random_word.get('english'):
-      print("Incorrect! Answer: " + random_word.get("english"))   
+      print("Incorrect! Answer: " + random_word.get("english")+ "\n")
+      print("Would you like to add this word to your dictionary?")
+      userInput = input("Press 'y' for yes and 'n' for no: ")
+      if userInput == 'y':
+        BaseUser.update_my_vocab(self, ger_word, random_word.get('english'))
+        print("Word added!")
+    
+      
     else:
       quit()  
       
-  
-    
-    
-class Dictionary:
-  def __init__(self, vocab):
-    self.vocab = vocab
-    
-    def add(self):
-      pass
-    
-    def remove(self):
-      pass
-
-class Flashcard:
-  pass
-
-class Player:
-  pass
 
 
 class Menu:
-  def __init__(self, vocab):
+  def __init__(self, user, vocab):
+    self.user = user
     self.vocab = vocab
+    
     
     print("Welcome to the German Quiz!")
     
@@ -62,10 +144,10 @@ class Menu:
       print("2. Update Dictionary")
       print("3. Quit")
       
-      choice = input("What would you like to do? ")
+      choice = input("What would you like to do? Select a number: ")
       
       if choice == '1':
-        game1 = Quiz(my_words)   
+        game1 = Quiz(my_words, user)   
         game1.random_word()
       elif choice == '2':
         pass
@@ -77,7 +159,19 @@ class Menu:
       
       
 
-  
-  
+regular_user = RegularUser("John", data_directory)
+premium_user = PremiumUser("Sarah", data_directory)
+print(regular_user)
+print(premium_user)
+print(regular_user.print_my_vocab())
+# regular_user.add_word("Handy", "phone")
 
-player1 = Menu(my_words)      
+
+
+player1 = Menu(regular_user, my_words)  # For a regular user
+
+
+# print(player1)
+# or
+# player1 = Menu(premium_user)  # For a premium user
+   
