aab3349587928a76846a6af814485dff5bf7ae86
pmk7 <72909830+pmk7@users.noreply.github.com>
2023-04-25 15:57:53 +0100

    removed docstrings, fixed bug

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ if not os.path.exists(data_directory):
 
 
 class BaseUser:
-    """A base class for users. Handles vocabulary management and storage."""
-
     def __init__(self, username, filepath):
-        """Initialize a user with a username and a file path for storing vocabulary."""
         self.username = username
         self.filepath = f"{filepath}/{self.username}.json"
 
@@ -26,56 +23,48 @@ class BaseUser:
         else:
             self.load_vocab()
 
-        def load_vocab(self):
-            """Load the user's vocabulary from a JSON file."""
-            with open(self.filepath, 'r') as file:
-                self.vocab = json.load(file)
+    def load_vocab(self):
+        with open(self.filepath, 'r') as file:
+            self.vocab = json.load(file)
 
-        def save_vocab(self):
-            """Save the user's vocabulary to a JSON file."""
-            with open(self.filepath, 'w') as file:
-                json.dump(self.vocab, file, ensure_ascii=False)
+    def save_vocab(self):
+        with open(self.filepath, 'w') as file:
+            json.dump(self.vocab, file)
 
-        def print_my_vocab(self):
-            """Print the user's vocabulary."""
-            print(self.vocab)
+    def print_my_vocab(self):
+        print(self.vocab)
 
-        def find_word(self, german):
-            """Find a word in the user's vocabulary by its German form."""
-            print(self.vocab)
-            for word in self.vocab:
-                if word['german'] == german:
-                    return word
-                return None
+    def find_word(self, german):
+        print(self.vocab)
+        for word in self.vocab:
+            if word['german'] == german:
+                return word
+        return None
 
-        def add_word(self, german, english):
-            """Add a word to the user's vocabulary."""
-            if self.find_word(german):
-                print('Already contained!')
-            else:
-                word_id = len(self.vocab) + 1
-                word = {'id': word_id, 'german': german,
-                        'english': english, 'correct': 0, 'incorrect': 0}
-                self.vocab.append(word)
-                self.save_vocab()
-
-        def remove_word(self, german):
-            """Remove a word from the user's vocabulary by its German form."""
-            word = self.find_word(german)
-            if word:
-                self.vocab.remove(word)
-                self.save_vocab()
-
-        def update_my_vocab(self, german, english):
-            """Update the user's vocabulary with a new word."""
+    def add_word(self, german, english):
+        if self.find_word(german):
+            print('Already contained!')
+        else:
             word_id = len(self.vocab) + 1
             word = {'id': word_id, 'german': german,
                     'english': english, 'correct': 0, 'incorrect': 0}
-            # Fix problem. Add incorrect word to users dictionary
+            self.vocab.append(word)
+            self.save_vocab()
 
-        def __str__(self):
-            """Return a string representation of the user."""
-            return f"User: {self.username}, Vocab: {len(self.vocab)}"
+    def remove_word(self, german):
+        word = self.find_word(german)
+        if word:
+            self.vocab.remove(word)
+            self.save_vocab()
+
+    def update_my_vocab(self, german, english):
+        word_id = len(self.vocab) + 1
+        word = {'id': word_id, 'german': german,
+                'english': english, 'correct': 0, 'incorrect': 0}
+        self.save_vocab()
+
+    def __str__(self):
+        return f"User: {self.username}, Vocab: {len(self.vocab)}"
 
 
 class RegularUser(BaseUser):
@@ -147,7 +136,7 @@ class Menu:
         while True:
             print("\n1. Quiz\n")
             print("\n2. Edit my Dictionary\n")
-            print("\n3. Revise My Dictionary\n")
+            print("\n3. Study My Dictionary\n")
             print("\n4. Quit\n")
 
             choice = input("What would you like to do? Select a number: ")
@@ -168,11 +157,11 @@ class Menu:
 
 regular_user = RegularUser("John", data_directory)
 premium_user = PremiumUser("Sarah", data_directory)
-print(regular_user)
-print(premium_user)
-print(regular_user.print_my_vocab())
+# print(regular_user)
+# print(premium_user)
+# print(regular_user.print_my_vocab())
 # regular_user.add_word("Handy", "phone")
-regular_user.remove_word("Haus")
+# regular_user.remove_word("Haus")
 
 
 player1 = Menu(regular_user, my_words)  # For a regular user
@@ -180,4 +169,4 @@ player1 = Menu(regular_user, my_words)  # For a regular user
 
 # print(player1)
 # or
-# player1 = Menu(premium_user)  # For a premium user
+# player1 = Menu(premium_user, my_words)  # For a premium user
